a3fb8a228f5e5a3d195f1ab4a550d9399ace7fd7
Luke Yost <lukexyost@gmail.com>
2022-06-14 13:25:03 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
   </main>
   <footer>
     <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+      🐦Made by Luke Yost at CodeWorks🐦
     </div>
   </footer>
 </template>
